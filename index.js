ef5d2366764a0549715aee0a1fe68c0fa2014979
Srijan1212 <153896279+Srijan1212@users.noreply.github.com>
2024-01-13 15:59:27 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const myButton =document.getElementById("myButton");
-const myLabell =document.getElementById("myLabell");
-const myLabel2 =document.getElementById("myLabel2");
-const myLabel3 =document.getElementById("myLabel3");
-const min=1;
-const max=6;
-let ranum1;
-let ranum2;
-let ranum3;
-
-myButton.onclick=function(){
-    ranum1=Math.floor(Math.random()*max)+min;
-    ranum2=Math.floor(Math.random()*max)+min;
-    ranum3=Math.floor(Math.random()*max)+min;
-    Label1.textContent=ranum1;
-    Label2.textContent=ranum2;
-    Label3.textContent=ranum3;
+const mytext = document.getElementById("mytext");
+const mysubmit = document.getElementById("mysubmit");
+const resultElement = document.getElementById("resultElement");
+let age;
+mysubmit.onclick=function(){
+    age=mytext.value;
+    age=Number(age);
+if (age>=100){
+    resultElement.textContent=`You are too old`;
 }
+else if(age==0){
+    resultElement.textContent=`You were just born`;
+}
+else if(age<=0){
+    resultElement.textContent=`Invalid`;
+}
+else if(age<=18){
+    resultElement.textContent=`you cannot vote`;
+}
+else if(age>=18){
+    resultElement.textContent=`You cn vote`;
+}
+   }
