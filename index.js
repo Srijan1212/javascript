181ee50e6437e4d3fa58eeeb1ee18ae01f726134
Srijan1212 <153896279+Srijan1212@users.noreply.github.com>
2024-01-13 18:54:13 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const myButton =document.getElementById("myButton");
-const myLabell =document.getElementById("myLabell");
-const myLabel2 =document.getElementById("myLabel2");
-const myLabel3 =document.getElementById("myLabel3");
-const min=1;
-const max=6;
-let ranum1;
-let ranum2;
-let ranum3;
+const mycheckbox= document.getElementById("mycheckbox");
+const visa= document.getElementById("visa");
+const paypal= document.getElementById("paypal");
+const mastercard= document.getElementById("mastercard");
+const btn= document.getElementById("btn");
+const subbutton= document.getElementById("subbutton");
+const paymentresult= document.getElementById("paymentresult");
 
-myButton.onclick=function(){
-    ranum1=Math.floor(Math.random()*max)+min;
-    ranum2=Math.floor(Math.random()*max)+min;
-    ranum3=Math.floor(Math.random()*max)+min;
-    Label1.textContent=ranum1;
-    Label2.textContent=ranum2;
-    Label3.textContent=ranum3;
+btn.onclick=function(){
+    if(mycheckbox.checked){
+        subbutton.textContent="You have subscribed"
+    }
+    else{
+        subbutton.textContent="You have not subscribed" 
+    }
+    if(visa.checked){
+        paymentresult.textContent="payment with visa card"
+    }
+    else if(mastercard.checked){
+        paymentresult.textContent="payment with mastercard"
+    }
+    else if(paypal.checked){
+            paymentresult.textContent="payment with paypal"
+        }
+    else{
+        paymentresult.textContent="error,you gotta pay otherwise fuck off"
+    }
+
+
 }
