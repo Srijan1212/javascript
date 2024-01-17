76a5275e4a2ec247f5bdc19e9255728d49167f0f
Srijan1212 <153896279+Srijan1212@users.noreply.github.com>
2024-01-17 16:07:20 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const textbox = document.getElementById("textbox")
-const Tofahrenheit = document.getElementById("Tofahrenheit")
-const ToCelcius = document.getElementById("ToCelcius")
-const result= document.getElementById("result")
-let temp;
-function convert(){
-    if(Tofahrenheit.checked){
-        temp=(textbox.value);
-        temp =temp* 9/5 +32;
-        result.textContent=temp+"°F "
+
+function rollit() {
+    const idunno = document.getElementById("idunno").value;
+    const result = document.getElementById("result");
+    const images = document.getElementById("ïmages");
+    const values = [];
+    const diceImages = []; 
+
+    for (let i = 0; i < idunno; i++) {
+        const value = Math.ceil(Math.random() * 6);
+        values.push(value);
+        diceImages.push(`<img src="dice ${value}.png" alt="Dice${value}">`);
     }
-    else if(ToCelcius.checked){ 
-        temp=(textbox.value);
-        temp =(temp-32)*(5/9)
-        result.textContent=temp+"°C"
-    }
-    else{
-        result.textContent="Select a unit";
-    }
+
+    result.textContent = `Dice: ${values.join(', ')}`;
+    images.innerHTML = diceImages.join('');
 }
